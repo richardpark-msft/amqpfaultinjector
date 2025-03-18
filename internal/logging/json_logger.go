a0cd689f5c46fbd7ec3460382f23ae7b5a087aab
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2025-03-18 22:42:50 +0000

    Adding doc comment.

--- internal/logging/json_logger.go
+++ internal/logging/json_logger.go
@@ -80,6 +80,7 @@ type JSONLine struct {
 	// Metadata are extra properties that can be added by logging implementations.
 	Metadata any `json:",omitempty"`
 
+	// Frame is the actual AMQP Frame object, including the Header and the Body.
 	Frame *frames.Frame
 
 	MessageData JSONMessageData `json:",omitempty"`
